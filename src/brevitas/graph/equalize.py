b252df9ea0607734c531ec2cd66bbc5c580c02f9
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-03-15 01:58:42 +0000

    Fix (graph_eq): fix for early exit threshold

--- src/brevitas/graph/equalize.py
+++ src/brevitas/graph/equalize.py
@@ -292,12 +292,11 @@ def _equalize(model: GraphModule, regions: Set[Tuple[str]], iterations: int, thr
         scale_factor_max = None
         for region in regions:
             scale_factors_region = _cross_layer_equalization([name_to_module[n] for n in region[0]], [name_to_module[n] for n in region[1]], merge_bias, bias_shrinkage, scale_computation_type)
-
-        scale_factor_region_max = torch.max(torch.abs(1 - scale_factors_region))
-        if scale_factor_max is not None:
-            scale_factor_max = torch.max(scale_factor_max, scale_factor_region_max)
-        else:
-            scale_factor_max = scale_factor_region_max
+            scale_factor_region_max = torch.max(torch.abs(1 - scale_factors_region))
+            if scale_factor_max is not None:
+                scale_factor_max = torch.max(scale_factor_max, scale_factor_region_max)
+            else:
+                scale_factor_max = scale_factor_region_max
         if threshold is not None and scale_factor_max < threshold:
             break
     return model
