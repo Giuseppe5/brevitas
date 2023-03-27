191206bb6370875ff0065d668f6af72ea6708a23
Giuseppe Franco <giuseppefranco4@gmail.com>
2023-03-27 10:39:08 +0000

    Test (graph_eq): tests for equalized bn

--- tests/brevitas/graph/test_equalization.py
+++ tests/brevitas/graph/test_equalization.py
@@ -1,60 +1,101 @@
 # Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause
 
-from packaging import version
-import pytest
+import copy
+
 import torch
 from torchvision import models
 
-from brevitas import torch_version
 from brevitas.fx import symbolic_trace
-from brevitas.graph.equalize import _cross_layer_equalization
 from brevitas.graph.equalize import _extract_regions
 from brevitas.graph.equalize import _is_supported_module
+from brevitas.graph.utils import get_module
 
 from .equalization_fixtures import *
 
-SEED = 123456
-ATOL = 1e-3
 
-
-def equalize_test(model, regions, merge_bias, bias_shrinkage, scale_computation_type):
-    name_to_module = {}
-    name_set = {name for region in regions for module_set in region for name in module_set}
-    scale_factors_regions = []
-    for name, module in model.named_modules():
-        if name in name_set:
-            name_to_module[name] = module
-    for region in regions:
-        scale_factors_region = _cross_layer_equalization([name_to_module[n] for n in region[0]], [name_to_module[n] for n in region[1]], merge_bias, bias_shrinkage, scale_computation_type)
-        scale_factors_regions.append(scale_factors_region)
-    return scale_factors_regions
-
-
-@pytest_cases.parametrize("model_dict", [(model_name, coverage) for model_name, coverage in MODELS.items()], ids=[ model_name for model_name, _ in MODELS.items()])
-@pytest.mark.parametrize("merge_bias", [True, False])
-def test_equalization_torchvision_models(model_dict: dict, merge_bias: bool):
-    model_name, coverage = model_dict
-
-    if model_name == 'googlenet' and torch_version == version.parse('1.8.1'):
-        pytest.skip('Skip because of PyTorch error = AttributeError: \'function\' object has no attribute \'GoogLeNetOutputs\' ')
-    if 'vit' in model_name and torch_version < version.parse('1.13'):
-        pytest.skip(f'ViT supported from torch version 1.13, current torch version is {torch_version}')
-
-    try:
-        model = getattr(models, model_name)(pretrained=True, transform_input=False)
-    except TypeError:
-        model = getattr(models, model_name)(pretrained=True)
+def test_load_state_dict_equalizedbn():
+    model = models.resnet18(pretrained=True)
 
     torch.manual_seed(SEED)
     inp = torch.randn(IN_SIZE_CONV)
     model.eval()
+    model = symbolic_trace(model)
     expected_out = model(inp)
 
+    model_orig = copy.deepcopy(model)
+    regions = _extract_regions(model)
+    _ = equalize_test(
+        model, regions, merge_bias=False, bias_shrinkage='vaiq', scale_computation_type='maxabs')
+    out = model(inp)
+
+    # Check that equalization is not introducing FP variations
+    assert torch.allclose(expected_out, out, atol=ATOL)
+
+    regions = sorted(regions, key=lambda region: region[0][0])
+    resnet_18_regions = sorted(RESNET_18_REGIONS, key=lambda region: region[0][0])
+    equalized_layers = set()
+    for region in resnet_18_regions:
+        equalized_layers.update(region[0])
+        equalized_layers.update(region[1])
+
+    # Check that we found all the expected regions
+    for region, expected_region in zip(regions, resnet_18_regions):
+        sources_check = set(region[0]) == set(expected_region[0])
+        sinks_check = set(region[1]) == set(expected_region[1])
+        assert sources_check
+        assert sinks_check
+
+    # Check that all BatchNorm were equalized
+    for module in model.modules():
+        if isinstance(module, nn.BatchNorm2d):
+            assert hasattr(module, 'orig_bias')
+
+    # Check that all layers were equalized and weights changed
+    for layer in equalized_layers:
+        eq_module = get_module(model, layer)
+        orig_module = get_module(model_orig, layer)
+        assert not torch.allclose(eq_module.weight, orig_module.weight)
+
+    # We re-load the state dict of the equalized model
+    model.load_state_dict(model.state_dict())
+    for module in model.modules():
+        if hasattr(module, 'orig_weight') and hasattr(module, 'orig_bias'):
+            # For an equalized BN, the equalized weight and the original weights should be different
+            assert not torch.allclose(module.weight, module.orig_weight)
+    out = model(inp)
+    assert torch.allclose(expected_out, out, atol=ATOL)
+    # We re-load the original non-equalized state dict
+    model.load_state_dict(model_orig.state_dict())
+    for module in model.modules():
+        if hasattr(module, 'orig_weight') and hasattr(module, 'orig_bias'):
+            # In this case, all equalization parameters should be set to 1 (i.e., no-op)
+            assert torch.allclose(module.scaling_factors, torch.tensor(1.))
+            assert torch.allclose(module.inverse_scaling_factors, torch.tensor(1.))
+            # The weight and the original weight must be the same
+            assert torch.allclose(module.weight, module.orig_weight)
+
+
+@pytest_cases.parametrize("merge_bias", [True, False])
+def test_equalization_torchvision_models(model_coverage: tuple, merge_bias: bool):
+    model, coverage = model_coverage
+
+    torch.manual_seed(SEED)
+    inp = torch.randn(IN_SIZE_CONV)
+    model.eval()
+    # The isistance does not work after symbolic trace
+    is_alexnet = isinstance(model, models.AlexNet)
     model = symbolic_trace(model)
 
+    expected_out = model(inp)
+
     regions = _extract_regions(model)
-    scale_factor_regions = equalize_test(model, regions, merge_bias=merge_bias, bias_shrinkage='vaiq', scale_computation_type='maxabs')
+    scale_factor_regions = equalize_test(
+        model,
+        regions,
+        merge_bias=merge_bias,
+        bias_shrinkage='vaiq',
+        scale_computation_type='maxabs')
     shape_scale_regions = [scale.shape for scale in scale_factor_regions]
 
     out = model(inp)
@@ -76,7 +117,7 @@ def test_equalization_torchvision_models(model_dict: dict, merge_bias: bool):
     # Graph equalization can exit in case of shape mismatches or other error without performing any
     # equalization and returning a scalar value. We check that the equalized regions are as many as
     # expected
-    if 'alexnet' in model_name:
+    if is_alexnet:
         # In AlexNet, we cannot equalize only through one region
         assert sum([shape == () for shape in shape_scale_regions]) == 1
     else:
@@ -84,47 +125,40 @@ def test_equalization_torchvision_models(model_dict: dict, merge_bias: bool):
 
 
 # Test that if we change BN stats, the bias value (which is dependant on them) also changes.
-@pytest_cases.parametrize("model_dict", [(model_name, coverage) for model_name, coverage in MODELS.items()], ids=[ model_name for model_name, _ in MODELS.items()])
-@pytest.mark.parametrize("merge_bias", [True, False])
-def test_equalization_torchvision_models_bn_stats(model_dict: dict, merge_bias: bool):
-    model_name, coverage = model_dict
-
-    if model_name == 'googlenet' and torch_version == version.parse('1.8.1'):
-        pytest.skip('Skip because of PyTorch error = AttributeError: \'function\' object has no attribute \'GoogLeNetOutputs\' ')
-    if 'vit' in model_name and torch_version < version.parse('1.13'):
-        pytest.skip(f'ViT supported from torch version 1.13, current torch version is {torch_version}')
-
-    try:
-        model = getattr(models, model_name)(pretrained=True, transform_input=False)
-    except TypeError:
-        model = getattr(models, model_name)(pretrained=True)
-
+@pytest_cases.parametrize("merge_bias", [True, False])
+def test_bn_stats_torchvision_models(model_coverage: tuple, merge_bias: bool):
+    model, _ = model_coverage
     torch.manual_seed(SEED)
     model.eval()
     model = symbolic_trace(model)
 
     regions = _extract_regions(model)
-    _ = equalize_test(model, regions, merge_bias=merge_bias, bias_shrinkage='vaiq', scale_computation_type='maxabs')
+    _ = equalize_test(
+        model,
+        regions,
+        merge_bias=merge_bias,
+        bias_shrinkage='vaiq',
+        scale_computation_type='maxabs')
 
     pre_bn_stats = []
-    for _, modules in model.named_modules():
-        # if isinstance(modules,(nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)):
-        if hasattr(modules, 'orig_bias'):
-            modules.train()
-            pre_bn_stats.append(modules.bias.data.clone())
-
-    for _ in range(100):
-        model(torch.randn(IN_SIZE_CONV))
-
     post_bn_stats = []
-    for _, modules in model.named_modules():
-        if hasattr(modules, 'orig_bias'):
-            post_bn_stats.append(modules.bias.data.clone())
+    for _, module in model.named_modules():
+        # Check for Equalized BN where scaling factors did not converge to one.
+        # If they are all one, then the bias is no longer a function of running mean and running_var
+        if hasattr(module, 'orig_bias') and not torch.allclose(
+                module.scaling_factors, torch.ones_like(module.scaling_factors)):
+            module.train()
+            pre_bn_stats.append(module.bias.data.clone())
+            # Simulate big changes in statistics otherwise we might get false negative in the tests
+            module.running_mean.fill_(100)
+            module.running_var.fill_(500)
+            post_bn_stats.append(module.bias.data.clone())
 
     for pre_val, post_val in zip(pre_bn_stats, post_bn_stats):
         assert not torch.allclose(pre_val, post_val, atol=ATOL)
 
-@pytest.mark.parametrize("merge_bias", [True, False])
+
+@pytest_cases.parametrize("merge_bias", [True, False])
 def test_models(toy_model, merge_bias, request):
     test_id = request.node.callspec.id
 
@@ -141,7 +175,12 @@ def test_models(toy_model, merge_bias, request):
     expected_out = model(inp)
     model = symbolic_trace(model)
     regions = _extract_regions(model)
-    scale_factor_regions = equalize_test(model, regions, merge_bias=merge_bias, bias_shrinkage='vaiq', scale_computation_type='maxabs')
+    scale_factor_regions = equalize_test(
+        model,
+        regions,
+        merge_bias=merge_bias,
+        bias_shrinkage='vaiq',
+        scale_computation_type='maxabs')
     shape_scale_regions = [scale.shape for scale in scale_factor_regions]
 
     out = model(inp)
